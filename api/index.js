635129ddf2bb9c74ddedf00e2141cb8ab820fe89
raulvictorrosa <raulvictorrosa@gmail.com>
2018-02-15 17:31:59 -0200

    feat: functions of the api

--- api/index.js
+++ api/index.js
@@ -1,34 +1,60 @@
 import { AsyncStorage } from 'react-native'
 
-export const CALENDAR_STORAGE_KEY = 'UdaciFitness:calendar'
+export const FLASHCARDS_STORAGE_KEY = 'MobileFlashcards:decks'
 
-// function formatCalendarResults(results) {
-//   return results === null
-//     ? setDummyData()
-//     : setMissingDates(JSON.parse(results))
-// }
+export function getDecks() {
+  return AsyncStorage.getAllKeys(FLASHCARDS_STORAGE_KEY).then(keys => {
+    return AsyncStorage.multiGet(keys).then(stores => {
+      return stores.map((result, i, store) => {
+        // get at each store's key/value so you can work with it
+        let key = store[i][0];
+        let value = JSON.parse(store[i][1]);
+        if (value) {
+          return {
+            key,
+            title: value.title,
+            questions: value.questions
+          };
+        }
+      }).filter(items => {
+        if (items) {
+          return typeof items.questions !== 'undefined'
+        }
+      })
+    })
+  })
 
-// export function fetchDecks() {
-//   return AsyncStorage.getItem(CALENDAR_STORAGE_KEY)
-//     .then(
-//       results === null
-//         ? setDummyData()
-//         : setMissingDates(JSON.parse(results))
-//     )
-// }
+  // AsyncStorage.getAllKeys((err, keys) => {
+  //   AsyncStorage.multiGet(keys, (err, stores) => {
+  //     stores.map((result, i, store) => {
+  //       // get at each store's key/value so you can work with it
+  //       let key = store[i][0];
+  //       let value = store[i][1];
+  //     });
+  //   });
+  // });
+}
+
+export function addDeck(title) {
+  try {
+    return AsyncStorage.setItem(title, JSON.stringify({ title, questions: [] }));
+  } catch (error) {
+    console.log(error);
+  }
+}
 
 export function submitEntry({ deck, key }) {
-  return AsyncStorage.mergeItem(CALENDAR_STORAGE_KEY, JSON.stringify({
+  return AsyncStorage.mergeItem(FLASHCARDS_STORAGE_KEY, JSON.stringify({
     [key]: deck
   }))
 }
 
 export function removeEntry(key) {
-  return AsyncStorage.getItem(CALENDAR_STORAGE_KEY)
+  return AsyncStorage.getItem(FLASHCARDS_STORAGE_KEY)
     .then((results) => {
-      const data = JSON.parse(results)
-      data[key] = undefined
-      delete data[key]
-      AsyncStorage.setItem(CALENDAR_STORAGE_KEY, JSON.stringify(data))
+      const deck = JSON.parse(results)
+      deck[key] = undefined
+      delete deck[key]
+      AsyncStorage.setItem(FLASHCARDS_STORAGE_KEY, JSON.stringify(data))
     })
 }
