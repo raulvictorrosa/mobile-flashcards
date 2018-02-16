0f4dd07146570ff0c3d82e46e6a3adebf1d03eff
raulvictorrosa <raulvictorrosa@gmail.com>
2018-02-16 15:59:13 -0200

    fix: Fix functions fetchDecks and addDeck

--- api/index.js
+++ api/index.js
@@ -1,52 +1,26 @@
 import { AsyncStorage } from 'react-native'
 
-export const FLASHCARDS_STORAGE_KEY = 'MobileFlashcards:decks'
-
-export function getDecks() {
-  return AsyncStorage.getAllKeys(FLASHCARDS_STORAGE_KEY).then(keys => {
-    return AsyncStorage.multiGet(keys).then(stores => {
+export function fetchDecks() {
+  return AsyncStorage.getAllKeys().then(keys => {
+    return AsyncStorage.multiGet().then(stores => {
       return stores.map((result, i, store) => {
         // get at each store's key/value so you can work with it
         let key = store[i][0];
         let value = JSON.parse(store[i][1]);
-        if (value) {
-          return {
-            key,
-            title: value.title,
-            questions: value.questions
-          };
-        }
-      }).filter(items => {
-        if (items) {
-          return typeof items.questions !== 'undefined'
-        }
       })
     })
   })
-
-  // AsyncStorage.getAllKeys((err, keys) => {
-  //   AsyncStorage.multiGet(keys, (err, stores) => {
-  //     stores.map((result, i, store) => {
-  //       // get at each store's key/value so you can work with it
-  //       let key = store[i][0];
-  //       let value = store[i][1];
-  //     });
-  //   });
-  // });
 }
 
-export function addDeck(title) {
+export function addDeck(deck) {
   try {
-    return AsyncStorage.setItem(title, JSON.stringify({ title, questions: [] }));
+    return AsyncStorage.setItem(
+      deck.title,
+      JSON.stringify( deck )
+    )
   } catch (error) {
     console.log(error);
   }
-}
-
-export function submitEntry({ deck, key }) {
-  return AsyncStorage.mergeItem(FLASHCARDS_STORAGE_KEY, JSON.stringify({
-    [key]: deck
-  }))
 }
 
 export function removeEntry(key) {
