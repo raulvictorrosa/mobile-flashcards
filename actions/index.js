c3d18d6a1449f71931b5f697753ebd9f44c18d2d
raulvictorrosa <raulvictorrosa@gmail.com>
2018-02-19 14:11:51 -0300

    feat: Action to fetch a specific deck

--- actions/index.js
+++ actions/index.js
@@ -1,9 +1,17 @@
 export const FETCH_DECKS = 'FETCH_DECKS'
+export const FETCH_DECK = 'FETCH_DECK'
 export const ADD_DECK = 'ADD_DECK'
 
 export function fetchDecks (decks) {
   return {
     type: FETCH_DECKS,
+    decks,
+  }
+}
+
+export function fetchDeck (deck) {
+  return {
+    type: FETCH_DECK,
     decks,
   }
 }
