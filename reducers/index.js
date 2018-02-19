e924df7ec1300e9187fca9987d1162948454c640
raulvictorrosa <raulvictorrosa@gmail.com>
2018-02-19 14:12:13 -0300

    feat: Reducer to fetch a specific deck

--- reducers/index.js
+++ reducers/index.js
@@ -1,9 +1,15 @@
-import { FETCH_DECKS, ADD_DECK } from '../actions'
+import { FETCH_DECKS, FETCH_DECK, ADD_DECK } from '../actions'
 
 function decks (state = {}, action) {
   switch (action.type) {
     case FETCH_DECKS :
       return action.decks
+
+    case FETCH_DECK :
+      return {
+        ...state,
+        ...action.deck
+      }
 
     case ADD_DECK :
       return [
