5f266e7e38962c5083a4f0034d9e3bcb0d906e36
raulvictorrosa <raulvictorrosa@gmail.com>
2018-02-19 14:13:19 -0300

    feat: Component to show a specific deck

--- components/DeckView.js
+++ components/DeckView.js
@@ -1,16 +1,55 @@
 import React, { Component } from 'react'
 import {
-  Text
+  Text,
+  View
 } from 'react-native'
 import { connect } from 'react-redux'
 import { NavigationActions } from 'react-navigation'
+import { black, white } from '../utils/colors'
 
 class DeckView extends Component {
+  static navigationOptions = ({ navigation }) => {
+    const { title } = navigation.state.params.deck
+
+    return {
+      title
+    }
+  }
+
   render() {
+    const { deck } = this.props
+    console.log(deck)
     return (
-      <Text>Deck View</Text>
+      <View>
+        <Text>Deck View - {deck.title}</Text>
+        <Text>Deck View - {deck.questions.length}</Text>
+      </View>
     )
   }
 }
 
-export default connect()(DeckView)
+function mapStateToProps(state, { navigation }) {
+  const { deck } = navigation.state.params
+
+  return {
+    deck
+  }
+}
+
+// function mapDispatchToProps(dispatch, { navigation }) {
+//   const { entryId } = navigation.state.params
+
+//   return {
+//     remove: () => dispatch(addEntry({
+//       [entryId]: timeToString() === entryId
+//         ? getDailyReminderValue()
+//         : null
+//     })),
+//     goBack: () => navigation.goBack(),
+//   }
+// }
+
+export default connect(
+  mapStateToProps,
+  // mapDispatchToProps,
+)(DeckView)
