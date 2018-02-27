15733faa03edc118299dc26d411b74bfe177663f
raulvictorrosa <raulvictorrosa@gmail.com>
2018-02-27 17:33:36 -0300

    feat: Change to new type of function

--- actions/index.js
+++ actions/index.js
@@ -3,31 +3,23 @@ export const FETCH_DECK = 'FETCH_DECK'
 export const ADD_DECK = 'ADD_DECK'
 export const ADD_CARD = 'ADD_CARD'
 
-export function fetchDecks (decks) {
-  return {
-    type: FETCH_DECKS,
-    decks,
-  }
-}
+export const fetchDecks = decks => ({
+  type: FETCH_DECKS,
+  decks
+})
 
-export function fetchDeck (deck) {
-  return {
-    type: FETCH_DECK,
-    deck,
-  }
-}
+export const fetchDeck = deck => ({
+  type: FETCH_DECK,
+  deck
+})
 
-export function addDeck (deck) {
-  return {
-    type: ADD_DECK,
-    deck,
-  }
-}
+export const addDeck = deck => ({
+  type: ADD_DECK,
+  deck
+})
 
-export function addCard (key, cards) {
-  return {
-    type: ADD_CARD,
-    key,
-    cards
-  }
-}
+export const addCard = (key, card) => ({
+  type: ADD_CARD,
+  key,
+  card
+})
